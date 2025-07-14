44cda27b3751c4dfec6c8ae89a75a435b5cfce29
ktvrt <cutbertomo@gmail.com>
2025-07-14 15:03:02 -0600

    wire:loading y formularios en crear y editar

--- resources/views/livewire/edit-post.blade.php
+++ resources/views/livewire/edit-post.blade.php
@@ -1,35 +1,43 @@
 <div>
     <!-- Modal editar posto -->    
     <flux:modal name="editar-post-modal" class="md:w-200">
-        <div class="space-y-6">
-            <div>
-                <flux:heading size="lg">Editar Post</flux:heading>
-                <flux:text class="mt-2">Ingresar los datos del Post.</flux:text>
+        <form wire:submit.prevent='actualizar'>
+            @csrf
+            <div class="space-y-6">
+                <div>
+                    <flux:heading size="lg">Editar Post</flux:heading>
+                    <flux:text class="mt-2">Ingresar los datos del Post.</flux:text>
+                </div>
+
+                <flux:input label="title" wire:model='title' placeholder="Titulo del Post" />
+
+                <flux:input label="imagen" wire:model='imagen' type="file" accept="image/*" placeholder="Imagen del Post" />
+                
+                
+                <div class="flex gap-2">
+                    <img src="{{ asset(Storage::url($imagenPost)) }}" @class([
+                        "w-12 h-12 rounded-2xl",
+                        "opacity-30" => $imagen,
+                    ]) >
+                    @if ($imagen) 
+                        <img src="{{ $imagen->temporaryUrl() }}" class="w-12 h-12 rounded-2xl" >
+                    @endif
+                </div>
+
+                <flux:textarea label="Body" wire:model='body' type="Body del Post" />
+
+                <div class="flex">
+                    <flux:spacer />
+                    <flux:button type="submit" variant="primary" >Actualizar</flux:button>
+                </div>
+
+                <div wire:loading wire:target='actualizar' class="flex items-center gap-2"> 
+                    <flux:badge color="lime">
+                        Actualizando post...
+                    </flux:badge>
+                </div>
             </div>
-
-            <flux:input label="title" wire:model='title' placeholder="Titulo del Post" />
-
-            <flux:input label="imagen" wire:model='imagen' type="file" accept="image/*" placeholder="Imagen del Post" />
-            
-            
-            <div class="flex gap-2">
-                <img src="{{ asset(Storage::url($imagenPost)) }}" @class([
-                    "w-12 h-12 rounded-2xl",
-                    "opacity-30" => $imagen,
-                ]) >
-                @if ($imagen) 
-                    <img src="{{ $imagen->temporaryUrl() }}" class="w-12 h-12 rounded-2xl" >
-                @endif
-            </div>
-
-            <flux:textarea label="Body" wire:model='body' type="Body del Post" />
-
-            <div class="flex">
-                <flux:spacer />
-
-                <flux:button type="submit" variant="primary" wire:click='actualizar'>Actualizar</flux:button>
-            </div>
-        </div>
+        </form>
     </flux:modal>
     <!-- fin Modal editar posto -->
 </div>
--- resources/views/livewire/post-crear.blade.php
+++ resources/views/livewire/post-crear.blade.php
@@ -1,29 +1,37 @@
 <div>
     <!-- Modal crear posto -->    
     <flux:modal name="crer-post-modal" class="md:w-200">
-        <div class="space-y-6">
-            <div>
-                <flux:heading size="lg">Crear Post</flux:heading>
-                <flux:text class="mt-2">Ingresar los datos del Post.</flux:text>
+        <form wire:submit.prevent='submit'>
+            @csrf
+            <div class="space-y-6">
+                <div>
+                    <flux:heading size="lg">Crear Post</flux:heading>
+                    <flux:text class="mt-2">Ingresar los datos del Post.</flux:text>
+                </div>
+
+                <flux:input label="title" wire:model='title' placeholder="Titulo del Post" />
+                
+                <flux:input label="imagen" wire:model='imagen' accept="image/*" type="file" placeholder="Imagen del Post" />
+                <div>
+                    @if ($imagen) 
+                        <img src="{{ $imagen->temporaryUrl() }}" class="w-12 h-12 rounded-2xl" >
+                    @endif
+                </div>
+
+                <flux:textarea label="Body" wire:model='body' type="Body del Post" />
+
+                <div class="flex">
+                    <flux:spacer />
+                    <flux:button type="submit" variant="primary" >Guardar</flux:button>
+                </div>
+
+                <div wire:loading wire:target='submit' class="flex items-center gap-2"> 
+                    <flux:badge color="lime">
+                        Guardando post...
+                    </flux:badge>
+                </div>
             </div>
-
-            <flux:input label="title" wire:model='title' placeholder="Titulo del Post" />
-            
-            <flux:input label="imagen" wire:model='imagen' accept="image/*" type="file" placeholder="Imagen del Post" />
-            <div>
-                @if ($imagen) 
-                    <img src="{{ $imagen->temporaryUrl() }}" class="w-12 h-12 rounded-2xl" >
-                @endif
-            </div>
-
-            <flux:textarea label="Body" wire:model='body' type="Body del Post" />
-
-            <div class="flex">
-                <flux:spacer />
-
-                <flux:button type="submit" variant="primary" wire:click='submit'>Guardar</flux:button>
-            </div>
-        </div>
+        </form>
     </flux:modal>
     <!-- fin Modal crear posto -->
 </div>
